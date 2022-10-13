53480b835198bd937d1e8c6cf303ad5177a4855f
tami <xorud7979@gmail.com>
2022-10-13 15:26:37 +0900

    feat: components추가 및 스타일 적용

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,39 +3,31 @@ import SummaryDash from "../components/SummaryDash";
 import { Calendar } from "../components/Calendar";
 import NavBar from "../components/NavBar";
 import NavigationList from "../components/NavigationList";
+import { useRouter } from "next/router";
 
 const OzicPay = () => {
   return (
     <>
-      <container>
+      <section>
         <div className="layout1">
-          <NavigationList />
+          
         </div>
         <div className="layout2">
-          <NavBar />
+          
           <main>
             <LocationDash />
             <SummaryDash />
             <Calendar />
           </main>
         </div>
-      </container>
+      </section>
 
       <style>
         {`
       main {
+        height: 100%;
         padding: 24px;
         background: rgb(238, 238, 238);
-      }
-      container {
-        width: 100%;
-        display: flex;
-      }
-      .layout1 {
-        flex: 1;
-      }
-      .layout2 {
-        flex: 6;
       }
       `}
       </style>

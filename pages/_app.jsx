d1be5c5eabecef33e351e501cd6603519f9de11c
tami <xorud7979@gmail.com>
2022-10-13 15:26:17 +0900

    feat: header, aside 고정시키기

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import NavigationList from "../components/NavigationList";
+import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="container">
+        <NavigationList className="NavigationList" />
+        <section>
+          <NavBar className="NavBar" />
+          <Component {...pageProps} className="Component" />
+        </section>
+      </div>
+
+      <style>
+        {`
+        .container {
+          display: flex;
+        }
+        section {
+          flex: 8;
+        }
+      `}
+      </style>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

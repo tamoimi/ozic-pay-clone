3aa900d22b25c8f8e3098b1780bd3aaf1c5f2429
tami <xorud7979@gmail.com>
2022-10-13 17:47:26 +0900

    feat: 대시보드 상단 내 상점 현황 데이터 가져오기

--- pages/api/index.jsx
+++ pages/api/index.jsx
@@ -1,13 +1,18 @@
+import { create } from "@mui/material/styles/createTransitions";
 import format from "date-fns";
 
 // SummaryDash date
-export const getTodayStatistics = async (searchDate: Date) => {
-  const url = `/dashboard/todayStatistics`;
+// 대시보드 상단 내 상점 현황 데이터를 가져올것
+
+// *key 를 숨기기 위해 & 서버?를 나누기 위해 .env파일에 키를 저장해서 불러와 사용함
+
+export const getTodayStatistics = async (searchDate) => {
+  const url = `${process.env.NEXT_APP_BASE_URL}/dashboard/todayStatistics`;
   const params = {
     searchYearMonth: format(searchDate, "yyyy-MM"),
   };
   const {
     data: { content },
-  } = await process.env.NEXT_APP_BASE_URL.post(url, params);
+  } = await fetch.post(url, params);
   return content;
 };

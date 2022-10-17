13485cdda9d0cb9335fbf9fd5ff1e1b156379f4e
tami <xorud7979@gmail.com>
2022-10-17 10:51:14 +0900

    feat: init

--- pages/Purchases.jsx
+++ pages/Purchases.jsx
@@ -1,5 +1,9 @@
 const Purchases = () => {
-    return <h1>Purchases page</h1>;
-  };
-  export default Purchases;
-  
+  return (
+    <>
+      <h1>Purchases page</h1>
+      <h1>매입 내역 페이지</h1>
+    </>
+  );
+};
+export default Purchases;

dd7ce5b6d22b49c2dd12e41a90382739f8bae33c
tami <xorud7979@gmail.com>
2022-10-17 16:43:04 +0900

    feat: 캘린더 하단 월 이동 기능 추가

--- components/Calendar/Calendar.jsx
+++ components/Calendar/Calendar.jsx
@@ -1,9 +1,47 @@
+import { addMonths, subMonths } from "date-fns";
 import { CalendarHeader } from "./CalendarHeader";
+import { CalendarBody } from "./CalendarBody";
+import { useQuery } from "@tanstack/react-query";
+import { getDailyStatistics } from "../../pages/api";
+import { Paper } from "@mui/material";
 
-export const Calendar = () => {
+export const Calendar = ({ currentMonth, setCurrentMonth }) => {
+  const goToPrevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+  const goToNextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+  const goToCurrentMonth = () => {
+    setCurrentMonth(new Date());
+  };
+
+  //data 조회 API
+  // const { data: cellData, isLoading } = useQuery(
+  //   ["/dashboard/dailyStatistics", currentMonth],
+  //   () => getDailyStatistics(currentMonth),
+  //   {
+  //     keepPreviousData: true,
+  //   }
+  // );
+
   return (
     <>
-      <CalendarHeader />
+      <Paper
+        elevation={4}
+        sx={{
+          paddingX: 3,
+          paddingY: 2,
+        }}
+      >
+        <CalendarHeader
+          currentMonth={currentMonth}
+          goToPrevMonth={goToPrevMonth}
+          goToNextMonth={goToNextMonth}
+          goToCurrentMonth={goToCurrentMonth}
+        />
+        <CalendarBody />
+      </Paper>
     </>
   );
 };

6b7e0e63de29c7a0eaca4b02a83ec268aaf921ed
tami <xorud7979@gmail.com>
2022-10-17 10:51:22 +0900

    feat: init

--- pages/Settlements.jsx
+++ pages/Settlements.jsx
@@ -1,4 +1,9 @@
 const Settlements = () => {
-  return <h1>Settlements page</h1>;
+  return (
+    <>
+      <h1>Settlements page</h1>
+      <h1>정산 내역 페이지</h1>
+    </>
+  );
 };
 export default Settlements;

0be9b4e4495e3d2ef5b9891a4d44a206dd1cd167
tami <xorud7979@gmail.com>
2022-10-18 11:06:56 +0900

    feat: atom에 state 추가

--- pages/recoil/atom.jsx
+++ pages/recoil/atom.jsx
@@ -1,8 +1,17 @@
-import { atom } from "recoil"
+import { atom } from "recoil";
 
-const clientDashboard = atom ({
-    key: 'clientDashboard',
-    default: 14,
-})
+const clientDashboard = atom({
+  key: "clientDashboard",
+  default: {
+    referenceDate: "",
+    totalPayment: 0,
+    expectedDepositAmount: 0, // 입금 예정 정산액
+    depositAmount: 0, // 입금 완료 정산액
+    isHoliday: false,
+    isComplete: false,
+    isCurrentMonth: false,
+    isOpen: false,
+  },
+});
 
 export default clientDashboard
